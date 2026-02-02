43624beff24542316c7e8ca93dd4bc7efe84acfd
Jake Goldsborough <jake.goldsborough@pm.me>
2026-02-01 18:08:33 -0800

    Minimal test - just user/group

--- services/cfgs-dev.nix
+++ services/cfgs-dev.nix
@@ -1,23 +1,7 @@
 { config, pkgs, ... }:
 
-let
-  cfgs-dev = pkgs.stdenv.mkDerivation rec {
-    pname = "cfgs-dev";
-    version = "20260201.0.0";
-
-    src = pkgs.fetchzip {
-      url = "https://github.com/ducks/cfgs.dev/releases/download/${version}/cfgs-dev.tar.gz";
-      hash = "sha256-13xq3biijhcnfvi5iqf231rb19vzab67fjm8i29hhn4w067gvf5c";
-      stripRoot = false;
-    };
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out/
-    '';
-  };
-
-in {
+{
+  # Minimal test - just create the user
   users.users.cfgs-dev = {
     isSystemUser = true;
     group = "cfgs-dev";
@@ -26,40 +10,4 @@ in {
   };
 
   users.groups.cfgs-dev = {};
-
-  systemd.tmpfiles.rules = [
-    "d /var/lib/cfgs-dev 0755 cfgs-dev cfgs-dev -"
-    "d /var/lib/cfgs-dev/app 0755 cfgs-dev cfgs-dev -"
-  ];
-
-  system.activationScripts.cfgs-dev = ''
-    mkdir -p /var/lib/cfgs-dev/app
-    rm -rf /var/lib/cfgs-dev/app/*
-    cp -r ${cfgs-dev}/* /var/lib/cfgs-dev/app/
-    chown -R cfgs-dev:cfgs-dev /var/lib/cfgs-dev
-  '';
-
-  systemd.services.cfgs-dev = {
-    description = "cfgs.dev";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      Type = "simple";
-      User = "cfgs-dev";
-      Group = "cfgs-dev";
-      WorkingDirectory = "/var/lib/cfgs-dev/app/.next/standalone";
-      ExecStart = "${pkgs.nodejs_22}/bin/node /var/lib/cfgs-dev/app/.next/standalone/server.js";
-      Restart = "on-failure";
-      RestartSec = "10s";
-
-      Environment = [
-        "NODE_ENV=production"
-        "PORT=3003"
-        "HOSTNAME=0.0.0.0"
-      ];
-
-      EnvironmentFile = "/var/lib/cfgs-dev/secrets.env";
-    };
-  };
 }
