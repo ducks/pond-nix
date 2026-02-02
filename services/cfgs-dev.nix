1d1d5de5b9acbd0f800af4298b021b1e70ef314f
Jake Goldsborough <jake.goldsborough@pm.me>
2026-02-01 18:14:33 -0800

    Try fetchzip directly without mkDerivation

--- services/cfgs-dev.nix
+++ services/cfgs-dev.nix
@@ -1,20 +1,10 @@
 { config, pkgs, ... }:
 
 let
-  cfgs-dev = pkgs.stdenv.mkDerivation rec {
-    pname = "cfgs-dev";
-    version = "20260201.0.0";
-
-    src = pkgs.fetchzip {
-      url = "https://github.com/ducks/cfgs.dev/releases/download/${version}/cfgs-dev.tar.gz";
-      hash = "sha256-13xq3biijhcnfvi5iqf231rb19vzab67fjm8i29hhn4w067gvf5c";
-      stripRoot = false;
-    };
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out/
-    '';
+  cfgs-dev = pkgs.fetchzip {
+    url = "https://github.com/ducks/cfgs.dev/releases/download/20260201.0.0/cfgs-dev.tar.gz";
+    hash = "sha256-13xq3biijhcnfvi5iqf231rb19vzab67fjm8i29hhn4w067gvf5c";
+    stripRoot = false;
   };
 
 in {
