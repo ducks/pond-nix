ff29e898bc2736959bbe2dd77ef3f347fb59c8b4
Jake Goldsborough <jake.goldsborough@pm.me>
2026-02-01 18:10:29 -0800

    Add derivation back

--- services/cfgs-dev.nix
+++ services/cfgs-dev.nix
@@ -1,7 +1,23 @@
 { config, pkgs, ... }:
 
-{
-  # Minimal test - just create the user
+let
+  cfgs-dev = pkgs.stdenv.mkDerivation rec {
+    pname = "cfgs-dev";
+    version = "20260201.0.0";
+
+    src = pkgs.fetchzip {
+      url = "https://github.com/ducks/cfgs.dev/releases/download/${version}/cfgs-dev.tar.gz";
+      hash = "sha256-13xq3biijhcnfvi5iqf231rb19vzab67fjm8i29hhn4w067gvf5c";
+      stripRoot = false;
+    };
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out/
+    '';
+  };
+
+in {
   users.users.cfgs-dev = {
     isSystemUser = true;
     group = "cfgs-dev";
